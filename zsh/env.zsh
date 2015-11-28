ccd85ff56524e41f2ba994ed7fb317a1930d7705
Joe Mizzi <themizzi@me.com>
2015-11-28 17:48:48 -0600

    Terminal colors

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,4 +1,6 @@
 export EDITOR='atom'
 export PATH="/usr/local/sbin:~/.rbenv/versions/2.2.2/bin/:$PATH"
+export CLICOLOR=1
+export LSCOLORS=GxFxCxDxBxegedabagaced
 fpath=(/usr/local/share/zsh-completions $fpath)
 cdpath=(~/Documents ~/Dropbox $cdpath)
