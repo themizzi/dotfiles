7bb943469aac97adb408b759937640cae8eb5270
Joe Mizzi <jmizzi@gorillagroup.com>
2016-10-04 16:28:57 -0500

    Using brew prefix for path

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,5 +1,5 @@
 export EDITOR='atom --wait'
-export PATH="/usr/local/sbin:~/.rbenv/versions/2.2.2/bin/:$PATH"
+export PATH="$(brew --prefix)/bin:$(brew --prefix):/sbin:~/.rbenv/versions/2.2.2/bin/:$PATH"
 export CLICOLOR=1
 export LSCOLORS=GxFxCxDxBxegedabagaced
 fpath=(/usr/local/share/zsh-completions $fpath)
