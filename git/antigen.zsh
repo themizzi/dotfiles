d16ee860c9ea191a6cf50772494ca5a818b06799
Joe Mizzi <themizzi@me.com>
2016-12-05 15:32:48 -0600

    updates from mac

--- git/antigen.zsh
+++ git/antigen.zsh
@@ -1,1 +1,2 @@
 antigen bundle robbyrussell/oh-my-zsh plugins/git
+antigen bundle robbyrussell/oh-my-zsh plugins/git-flow
--- zsh/antigen.zsh
+++ zsh/antigen.zsh
@@ -1,2 +1,8 @@
 antigen bundle zsh-users/zsh-syntax-highlighting
 antigen bundle zsh-users/zsh-history-substring-search
+# bind UP and DOWN arrow keys
+zmodload zsh/terminfo
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+antigen bundle zsh-users/zsh-autosuggestions
+bindkey '^ ' autosuggest-accept
