9fbd7518094775b4e3b196dadd3d34a002fb2617
Joe Mizzi <themizzi@me.com>
2016-12-06 23:52:32 -0600

    missing then

--- brew/linux/install.zsh
+++ brew/linux/install.zsh
@@ -1,4 +1,4 @@
-if [ -z $(which brew) ]
+if [ -z $(which brew) ]; then
   echo "  Installing Homebrew for you."
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
 fi
