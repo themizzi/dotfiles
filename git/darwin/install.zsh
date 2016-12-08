af6506167bc50abf9a5bc906fbe2c7a07dbe5296
Joe Mizzi <themizzi@me.com>
2016-12-08 14:10:28 -0600

    add tmux

--- git/darwin/install.zsh
+++ git/darwin/install.zsh
@@ -2,7 +2,7 @@ echo "[difftool \"sourcetree\"]
 	cmd = opendiff \"\$LOCAL\" \"\$REMOTE\"
 	path =
 [mergetool \"sourcetree\"]
-	cmd = /Applications/SourceTree.app/Contents/Resources/opendiff-w.sh \"\$LOCAL\" \"\$REMOTE\" -ancestor \"\$BASE\" -merge \"\y$MERGED\"
+	cmd = /Applications/SourceTree.app/Contents/Resources/opendiff-w.sh \"\$LOCAL\" \"\$REMOTE\" -ancestor \"\$BASE\" -merg$
 	trustExitCode = true
 [credential]
   helper = osxkeychain
