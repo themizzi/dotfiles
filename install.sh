909d5cef60793a63586b5e58507e005b402e0602
Joe Mizzi <themizzi@me.com>
2021-03-22 20:57:37 -0500

    feat: new and shiny dotfiles

---
+++ install.sh
@@ -0,0 +1,23 @@
+set -e
+
+# Homebrew
+
+## Install
+command -v brew 1> /dev/null || echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+test -d /usr/local/bin/brew && eval $(/usr/local/bin/brew shellenv) # Darwin
+test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv) # Linux
+
+## Setup Linux
+if command -v apt 1> /dev/null && ! apt list --installed 2> /dev/null | grep -q build-essential; then
+  sudo apt update
+  sudo apt install build-essential
+fi
+
+## Bundle
+brew bundle
+
+# Dotfiles
+
+ln -sf $(pwd)/.tmux.conf ${HOME}/.tmux.conf
+ln -sf $(pwd)/.nanorc ${HOME}/.nanorc
+ln -sf $(pwd)/.zshrc ${HOME}/.zshrc
