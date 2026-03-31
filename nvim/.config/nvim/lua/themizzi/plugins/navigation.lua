0253db7d546897a7f69cbe34984cd8338511f3aa
Joe Mizzi <themizzi@me.com>
2026-03-30 21:52:56 -0500

    chore: run stylua

--- nvim/.config/nvim/lua/themizzi/plugins/navigation.lua
+++ nvim/.config/nvim/lua/themizzi/plugins/navigation.lua
@@ -1,53 +1,53 @@
 return {
-  {
-    "nvim-tree/nvim-tree.lua",
-    cmd = { "NvimTreeToggle", "NvimTreeFocus", "NvimTreeFindFile" },
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("nvim-tree").setup({
-        git = {
-          enable = true,
-          ignore = false,
-        },
-        update_focused_file = {
-          enable = true,
-          update_root = false,
-        },
-        renderer = {
-          highlight_git = true,
-          icons = {
-            show = {
-              git = true,
-            },
-          },
-        },
-      })
-    end,
-  },
-  {
-    "nvim-tree/nvim-web-devicons",
-    lazy = true,
-  },
-  {
-    "ibhagwan/fzf-lua",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      local ok, fzf = pcall(require, "fzf-lua")
-      if ok then
-        fzf.setup({})
-      end
-    end,
-  },
-  {
-    "folke/flash.nvim",
-    event = "VeryLazy",
-    config = function()
-      local flash = require("flash")
+	{
+		"nvim-tree/nvim-tree.lua",
+		cmd = { "NvimTreeToggle", "NvimTreeFocus", "NvimTreeFindFile" },
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("nvim-tree").setup({
+				git = {
+					enable = true,
+					ignore = false,
+				},
+				update_focused_file = {
+					enable = true,
+					update_root = false,
+				},
+				renderer = {
+					highlight_git = true,
+					icons = {
+						show = {
+							git = true,
+						},
+					},
+				},
+			})
+		end,
+	},
+	{
+		"nvim-tree/nvim-web-devicons",
+		lazy = true,
+	},
+	{
+		"ibhagwan/fzf-lua",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			local ok, fzf = pcall(require, "fzf-lua")
+			if ok then
+				fzf.setup({})
+			end
+		end,
+	},
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		config = function()
+			local flash = require("flash")
 
-      flash.setup({})
+			flash.setup({})
 
-      vim.keymap.set({ "n", "x", "o" }, "s", flash.jump, { silent = true, desc = "Flash jump" })
-      vim.keymap.set({ "n", "x", "o" }, "S", flash.treesitter, { silent = true, desc = "Flash treesitter" })
-    end,
-  },
+			vim.keymap.set({ "n", "x", "o" }, "s", flash.jump, { silent = true, desc = "Flash jump" })
+			vim.keymap.set({ "n", "x", "o" }, "S", flash.treesitter, { silent = true, desc = "Flash treesitter" })
+		end,
+	},
 }
